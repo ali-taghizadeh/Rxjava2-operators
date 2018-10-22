c2faad10e5692be5ed8c62238a0ac8e4f9c6226b
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-22 23:52:08 +0330

    Implement some samples of JUST operator

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/creating/JustObservable.java
@@ -0,0 +1,109 @@
+package ir.taghizadeh.rxjava2_operators.creating;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * <h1>Just</h1>
+ * The Just operator converts an item into an Observable that emits that item.
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/just.html">Reactivex</a>
+ */
+
+public class JustObservable {
+
+    public void JustObservable_method1() {
+        /*
+
+        Here we convert a single string value into an observable that emits that string.
+
+        */
+        Observable.just("Pass a single string")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println(s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+                });
+    }
+
+    public void JustObservable_method2() {
+        /*
+
+        Here we convert 4 values into an observable that emits those values.
+        Also  remember that just operator cannot take more than 10 arguments.
+
+        */
+        Observable.just("Item", "1", "88.9", "Item2")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println(s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+                });
+    }
+
+    public void JustObservable_method3() {
+        /*
+
+        In this method we convert a list of strings into an observable.
+        Note that just operator will give you only 1 emission. This time a list.
+        In fact, just operator treats everything as an item regardless it is an array item or a string item.
+
+        */
+        Observable.just(new String[]{"Item1","Item2","Item3","Item4","Item5","Item6","Item7","Item8","Item9","Item10"})
+                .subscribe(new Observer<String[]>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String[] strings) {
+                        System.out.println(strings.length);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+                });
+    }
+}
