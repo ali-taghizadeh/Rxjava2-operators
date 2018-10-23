cf66567b020caeeefe490b89a7b52a06585613fa
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-23 22:14:24 +0330

    Implement a defer() operator example
    We only call Observable.just() when we requests the data
    But if you need the freshest data, defer() operator would be a great choice

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/creating/DeferObservable.java
@@ -0,0 +1,59 @@
+package ir.taghizadeh.rxjava2_operators.creating;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * <h1>Defer</h1>
+ * just(), from(), and other Observable creation tools store the value of data when created, not when subscribed
+ * But by using defer() operator, it does not create the Observable until the observer subscribes, and create a fresh Observable for each observer
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/just.html">Reactivex</a>
+ */
+
+public class DeferObservable {
+
+    private String value = "firstValue";
+
+    private Observable<String> valueObservable() {
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override
+            public ObservableSource<? extends String> call() throws Exception {
+                return Observable.just(value);
+            }
+        });
+    }
+
+    public void deferObservable() {
+
+        value = "secondValue";
+        Observable<String> stringObservable = valueObservable();
+
+        stringObservable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("DONE!");
+            }
+        });
+    }
+}
