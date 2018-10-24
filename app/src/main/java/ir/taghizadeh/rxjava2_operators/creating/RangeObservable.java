fc71dfce9fa4113dbf53335240e5766c022f7eaf
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-24 20:46:11 +0330

    Implement a range() operator example

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/creating/RangeObservable.java
@@ -0,0 +1,49 @@
+package ir.taghizadeh.rxjava2_operators.creating;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * <h1>Range</h1>
+ * create an Observable that emits a particular range of sequential integers
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/range.html">Reactivex</a>
+ */
+
+public class RangeObservable {
+
+    public void rangeObservable() {
+        /*
+
+        The function takes the starting number and length.
+        Here we have a starting number of 25 and a range of 10 numbers, so it will print values from 25 to 34.
+
+        */
+
+        Observable.range(25, 10)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+                });
+
+    }
+}
