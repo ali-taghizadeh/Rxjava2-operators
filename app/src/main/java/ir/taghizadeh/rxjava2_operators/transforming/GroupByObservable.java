56b4a8ad4c37d3d793057aed015ea6b98912e1a7
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-27 22:48:30 +0330

    Implement a groupBy() operator example

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/transforming/GroupByObservable.java
@@ -0,0 +1,78 @@
+package ir.taghizadeh.rxjava2_operators.transforming;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observables.GroupedObservable;
+
+/**
+ * <h1>GroupBy</h1>
+ * Divide an Observable into a set of Observables that each emit a different subset of items from the original Observable
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/groupby.html">Reactivex</a>
+ */
+
+public class GroupByObservable {
+
+    public void groupByObservable() {
+        /*
+
+        Here is an animation demonstrating this operator very well :
+        http://blogs.microsoft.co.il/iblogger/2015/08/11/animations-of-rx-operators-groupby
+
+        */
+
+        Observable.just(2,3,8,9,14,12)
+                .groupBy(new Function<Integer, Boolean>() {
+                    @Override
+                    public Boolean apply(Integer integer) throws Exception {
+                        return integer % 3 == 0;
+                    }
+                })
+                .subscribe(new Observer<GroupedObservable<Boolean, Integer>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(GroupedObservable<Boolean, Integer> booleanIntegerGroupedObservable) {
+                        if (booleanIntegerGroupedObservable.getKey()){
+                            booleanIntegerGroupedObservable.subscribe(new Observer<Integer>() {
+                                @Override
+                                public void onSubscribe(Disposable d) {
+
+                                }
+
+                                @Override
+                                public void onNext(Integer integer) {
+                                    System.out.println(integer);
+                                }
+
+                                @Override
+                                public void onError(Throwable e) {
+                                    e.printStackTrace();
+                                }
+
+                                @Override
+                                public void onComplete() {
+                                    System.out.println("DONE!");
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+                });
+    }
+}
