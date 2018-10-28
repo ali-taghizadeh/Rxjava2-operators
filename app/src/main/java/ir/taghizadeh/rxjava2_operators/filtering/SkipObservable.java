3618339121fafe19a6eb4cbaccdf3286586fc657
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-28 11:33:58 +0330

    Implement a skip() operator example

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/filtering/SkipObservable.java
@@ -0,0 +1,51 @@
+package ir.taghizadeh.rxjava2_operators.filtering;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * <h1>Skip</h1>
+ * Suppress the first n items emitted by an Observable
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/skip.html">Reactivex</a>
+ */
+
+public class SkipObservable {
+
+    public void skipObservable() {
+        /*
+
+        You can ignore the first n items emitted by an Observable and attend only to those items that come after,
+        by modifying the Observable with the Skip operator.
+
+        */
+
+        Observable.range(1,10)
+                .skip(5)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+
+                });
+
+    }
+}
