b2b3637bdb3fd96392335d0e1712723c3f173a36
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-31 23:02:33 +0330

    Implement a sequenceEqual() operator example

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/conditional/SequenceEqualObservable.java
@@ -0,0 +1,50 @@
+package ir.taghizadeh.rxjava2_operators.conditional;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * <h1>SequenceEqual</h1>
+ * Determine whether two Observables emit the same sequence of items
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/sequenceequal.html">Reactivex</a>
+ */
+
+public class SequenceEqualObservable {
+
+    public void sequenceEqualObservable() {
+        /*
+
+        Pass SequenceEqual two Observables, and it will compare the items emitted by each Observable,
+        and the Observable it returns will emit true only if both sequences are the same
+        (the same items, in the same order, with the same termination state).
+
+        */
+
+        Observable<Long> longObservable = Observable.interval(1, TimeUnit.SECONDS)
+                .map(aLong -> aLong * 2);
+        Observable<Long> longObservable1 = Observable.interval(2, TimeUnit.SECONDS)
+                .map(aLong -> aLong);
+        Observable.sequenceEqual(longObservable, longObservable1)
+                .subscribe(new SingleObserver<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(Boolean aBoolean) {
+                        System.out.println(aBoolean);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
