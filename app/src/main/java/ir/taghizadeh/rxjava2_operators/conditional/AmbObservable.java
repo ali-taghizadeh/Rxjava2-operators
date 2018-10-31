c8f54765546f42ef8b5fe9de61f243516ca189df
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-10-31 21:24:41 +0330

    Implement an amb() operator example

---
+++ app/src/main/java/ir/taghizadeh/rxjava2_operators/conditional/AmbObservable.java
@@ -0,0 +1,66 @@
+package ir.taghizadeh.rxjava2_operators.conditional;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+
+/**
+ * <h1>Amb</h1>
+ * Given two or more source Observables, emit all of the items from only the first of these Observables to emit an item or notification
+ *
+ * @author ALi Taghizadeh
+ * @see <a href="http://reactivex.io/documentation/operators/amb.html">Reactivex</a>
+ */
+
+public class AmbObservable {
+
+    public void ambObservable() {
+        /*
+
+        When you pass a number of source Observables to Amb, it will pass through the emissions and notifications of exactly one of these Observables:
+        the first one that sends a notification to Amb, either by emitting an item or sending an onError or onCompleted notification.
+        Amb will ignore and discard the emissions and notifications of all of the other source Observables.
+
+        */
+
+        Observable<Long> longObservable = Observable.interval(2, TimeUnit.SECONDS)
+                .map(new Function<Long, Long>() {
+                    @Override
+                    public Long apply(Long aLong) throws Exception {
+                        return aLong * 10;
+                    }
+                });
+        Observable<Long> longObservable1 = Observable.interval(1, TimeUnit.SECONDS)
+                .map(new Function<Long, Long>() {
+                    @Override
+                    public Long apply(Long aLong) throws Exception {
+                        return aLong * 5;
+                    }
+                });
+        Observable.ambArray(longObservable, longObservable1)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        System.out.println(aLong);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("DONE!");
+                    }
+                });
+    }
+}
